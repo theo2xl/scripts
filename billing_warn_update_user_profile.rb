794e3fccbaff054d7761010b435efb98f38293ce
Jon Conley & Ted Coleman <pair+jon.conley+ted.coleman@sendgrid.com>
2014-01-06 14:21:40 -0700

    Added some stuff for bill warn testing

--- billing_warn_update_user_profile.rb
+++ billing_warn_update_user_profile.rb
@@ -1,18 +1,15 @@
-require 'smarter_csv'
+require 'csv'
 
 class BillingWarnUpdateUserProfile
 
   def self.process(file_name)
-    raw_data = SmarterCSV.process(file_name, {:downcase_header => false, :strings_as_keys => true})
-    
-    billing_warn_array = Array.new(raw_data.length)
+    billing_warn_array = Array.new
 
-    raw_data.each do |declined_user|
-      billing_warn_array << declined_user['Subscription.UserId__c']
+    CSV.foreach("#{file_name}.csv") do |row|
+      billing_warn_array << row[0] unless billing_warn_array.include?(row[0])
     end
 
-p billing_warn_array.join(',')
-
+    p billing_warn_array.join(',')
   end
 
 end
